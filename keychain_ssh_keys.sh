68a213c80df543e0ecc2c7429ea8378d7026397a
Makoto Oda <truth_jp_4133@yahoo.co.jp>
2021-08-22 13:48:25 +0900

    fix not to load 'keychain_ssh_keys' automatically

--- keychain_ssh_keys.sh
+++ keychain_ssh_keys.sh
@@ -6,30 +6,27 @@ SHELL_RC_FILE_PATH=$HOME/.bashrc
 SRC_FILE_PATH=./file/keychain_ssh_keys
 DST_FILE_PATH=/etc/keychain_ssh_keys
 
-sudo cp -f "${SRC_FILE_PATH}" "${DST_FILE_PATH}"
-
-cat "${SHELL_RC_FILE_PATH}" | grep ". ${DST_FILE_PATH}" >/dev/null
+dpkg -l keychain | grep -E "^ii( )+keychain" >/dev/null
 if [ $? -ne 0 ]; then
-    echo -n "\n" >> "${SHELL_RC_FILE_PATH}"
-    echo ". ${DST_FILE_PATH}" >> "${SHELL_RC_FILE_PATH}"
-fi
-
-. "${SHELL_RC_FILE_PATH}"
-
-sudo apt list --installed | grep "keychain" >/dev/null
-KEYCHAIN_INSTALLED=$?
-sudo apt list --installed | grep "ssh-askpass" >/dev/null
-SSH_ASKPASS_INSTALLED=$?
-if [ ${KEYCHAIN_INSTALLED} -eq 0 -a ${SSH_ASKPASS_INSTALLED} -eq 0 ]; then
-    exit 0
-fi
-
-sudo apt -y update
-if [ ${KEYCHAIN_INSTALLED} -ne 0 ]; then
     sudo apt -y install keychain
 fi
-if [ ${SSH_ASKPASS_INSTALLED} -ne 0 ]; then
+dpkg -l ssh-askpass | grep -E "^ii( )+ssh-askpass" >/dev/null
+if [ $? -ne 0 ]; then
     sudo apt -y install ssh-askpass
+fi
+
+sudo cp -f "${SRC_FILE_PATH}" "${DST_FILE_PATH}"
+
+cat "${SHELL_RC_FILE_PATH}" | grep -E "^. ${DST_FILE_PATH}" >/dev/null
+if [ $? -ne 0 ]; then
+    echo -n -e "\n"
+    echo "In a case when you use keychain to store ssh private key pass phrase, please set the statements below on your shell init script."
+    echo "When you use keychain, you need also public keys corresponded to private keys."
+    echo -n -e "\n"
+    echo "[statements start]"
+    echo "export SSH_KEYS_LIST_FILE_PATH=<ssh_private_key_list_file_path>"
+    echo ". ${DST_FILE_PATH}"
+    echo "[statements end]"
 fi
 
 exit 0
